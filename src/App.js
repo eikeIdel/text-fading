3abe5bd138cdf0a46a2459da82ef2cb49385a6ac
Eike Idel <3ike.idel@gmail.com>
2022-05-29 19:38:33 +0200

    finish resusable fader component

--- src/App.js
+++ src/App.js
@@ -4,26 +4,67 @@ import TextOne from "./components/TextOne";
 import TextTwo from "./components/TextTwo";
 import TextThree from "./components/TextThree";
 import Fader from "./components/Fader";
+import PicOne from "./asses/picOne.jpg";
+import PicTwo from "./asses/picTwo.jpg";
+import PicThree from "./asses/picThree.jpg";
 
 function App() {
-  const [activeGroup, setActiveGroup] = useState(0);
+  const [activeGroup, setActiveGroup] = useState("groupOne");
 
   return (
     <div className="App">
       <div className="wrapper">
         <div className="button-container">
-          <button className="buttons" onClick={() => setActiveGroup(0)}>
+          <button
+            className="buttons"
+            onClick={() => setActiveGroup("groupOne")}
+          >
             First
           </button>
-          <button className="buttons" onClick={() => setActiveGroup(1)}>
+          <button
+            className="buttons"
+            onClick={() => setActiveGroup("groupTwo")}
+          >
             Second
           </button>
-          <button className="buttons" onClick={() => setActiveGroup(2)}>
+          <button
+            className="buttons"
+            onClick={() => setActiveGroup("groupThree")}
+          >
             Third
           </button>
         </div>
         <Fader activeGroup={activeGroup}>
-          {[<TextOne />, <TextTwo />, <TextThree />]}
+          {[
+            <TextOne group="groupOne" key="groupOne" />,
+            <TextTwo group="groupTwo" key="groupTwo" />,
+            <TextThree group="groupThree" key="groupThree" />,
+          ]}
+        </Fader>
+        <Fader activeGroup={activeGroup}>
+          {[
+            <img
+              src={PicOne}
+              alt="oen"
+              key="groupOne"
+              group="groupOne"
+              width="100%"
+            />,
+            <img
+              src={PicTwo}
+              alt="Twon"
+              key="groupTwo"
+              group="groupTwo"
+              width="100%"
+            />,
+            <img
+              src={PicThree}
+              alt="Three"
+              key="groupThree"
+              group="groupThree"
+              width="100%"
+            />,
+          ]}
         </Fader>
       </div>
     </div>
--- src/components/Fader/index.jsx
+++ src/components/Fader/index.jsx
@@ -1,18 +1,23 @@
 import { useState, useEffect, useRef } from "react";
 import { useTransition, animated } from "react-spring";
 
-//prop {children} is an array of components
-function Fader({ children }, { activeGroup }) {
-  const [active, setActive] = useState(0);
+//prop {children} is an array of objects -> [{group:'groupOne',comp:<Child/>},...]
+function Fader({ children, activeGroup }) {
+  const [active, setActive] = useState("groupOne");
   const [newActive, setNewActive] = useState(0);
 
   useEffect(() => {
+    // const groups = console.log(groups);
+    // const index = children.indexOf(
+    //   (child) => child.props.group === activeGroup
+    // );
+    // console.log(index);
     setNewActive(activeGroup);
     setActive(null);
   }, [activeGroup]);
 
   const transition = useTransition(active, {
-    key: active?.key,
+    key: activeGroup?.key,
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
@@ -21,17 +26,17 @@ function Fader({ children }, { activeGroup }) {
   });
 
   return (
-    <>
-      {transition((style, i) => (
+    <div>
+      {transition((style, item) => (
         <animated.div
           style={{
             ...style,
           }}
         >
-          {children[i]}
+          {children[children.map((child) => child.props.group).indexOf(item)]}
         </animated.div>
       ))}
-    </>
+    </div>
   );
 }
 
