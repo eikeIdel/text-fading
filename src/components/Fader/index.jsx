620bfd1a964d66007f7cff7e978ea4967a8198ad
Eike Idel <3ike.idel@gmail.com>
2022-05-30 15:41:33 +0200

    remove tniy boilderplate and add some comments

--- src/components/Fader/index.jsx
+++ src/components/Fader/index.jsx
@@ -1,28 +1,24 @@
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useTransition, animated } from "react-spring";
 
 //prop {children} is an array of objects -> [{group:'groupOne',comp:<Child/>},...]
 function Fader({ children, activeGroup }) {
   const [active, setActive] = useState("groupOne");
-  const [newActive, setNewActive] = useState(0);
 
   useEffect(() => {
-    // const groups = console.log(groups);
-    // const index = children.indexOf(
-    //   (child) => child.props.group === activeGroup
-    // );
-    // console.log(index);
-    setNewActive(activeGroup);
+    //set first active = null to fade out previous element.
     setActive(null);
   }, [activeGroup]);
 
   const transition = useTransition(active, {
-    key: activeGroup?.key,
+    key: activeGroup.key,
     from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
     config: { duration: 400 },
-    onRest: () => active === null && setActive(newActive),
+    //after previous element is faded out (with active == null),
+    //start animation again for the next element (active = activeGroup)
+    onRest: () => active === null && setActive(activeGroup),
   });
 
   return (
